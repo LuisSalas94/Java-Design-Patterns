57efc08ed4e549315e4dc235c8740d94dee4c843
Fernando Salas <luisfernandosalasg@gmail.com>
2023-08-09 09:49:43 -0500

    Create Interface Behaviours

---
+++ src/fernandosalas/designpatterns/strategy/example1/flybehavior/FlyBehavior.java
@@ -0,0 +1,5 @@
+package fernandosalas.designpatterns.strategy.example1.flybehavior;
+
+public interface FlyBehavior {
+    void fly();
+}
---
+++ src/fernandosalas/designpatterns/strategy/example1/quackbehavior/QuackBehavior.java
@@ -0,0 +1,5 @@
+package fernandosalas.designpatterns.strategy.example1.quackbehavior;
+
+public interface QuackBehavior {
+    void quack();
+}
